315db2761ef7cf44501425b9a3e596a302d85b9e
hemajayachandran <hema.jayachandran.a@gmail.com>
2020-08-05 21:14:21 +0530

    sample cpp

---
+++ simple_demo_src.1.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+	int a = 0, b=1, c=2;
+	
+	for (int i = 0; i <= 5; i++)
+	{
+		a = b * c;
+		c = b;
+		
+	}
+	
+}
---
+++ simple_demo_src.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+	int a = 0, b=1, c=2, d=3;
+	
+	for (int i = 0; i <= 5; i++)
+	{
+		a = ((b + c) * d);
+		d = b/2;
+		b = c + 2;
+		
+	}
+	
+}
